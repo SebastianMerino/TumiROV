fd0b0c0ae91923794400c5ae7a59af095a06d3fd
SebastianMerino <sebastian.merino@pucp.edu.pe>
2022-07-27 17:32:11 -0500

    modified:   pixhawk/__pycache__/vehicle.cpython-39.pyc 	new file:   pixhawk/navegacion.py 	new file:   pixhawk/static/tabla.js -m "Script para actualizar 	tabla" 	new file:   pixhawk/templates/ROV_1cam.html 	new file:   pixhawk/templates/navegacion.html

---
+++ pixhawk/navegacion.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, json
+from vehicle import Vehicle
+
+app = Flask(__name__)
+PX4 = Vehicle("COM4")
+
+@app.route("/")
+def ROV():
+	return render_template("ROV_1cam.html")
+
+@app.route("/navegacion")
+def navegacion():
+    return render_template("navegacion.html")
+
+@app.route("/datos_PX4")
+def datos_px4():
+	att_dict = dict(zip(['roll','pitch','yaw'],PX4.attitude))
+	v_dict = dict(zip(['vx','vy','vz'],PX4.velocity))
+	datos_dict = {'attitude':att_dict, 'velocity':v_dict, 'time_boot':PX4.time_boot}
+	return json.dumps(datos_dict)
+
+if __name__ == '__main__':
+	ip = "127.0.0.1"
+	p = 8000			# Puerto
+
+	PX4.start_data_rx()
+
+	# start the flask app
+	app.run(host=ip, port=p, debug=False, threaded=True, use_reloader=False)
+
+PX4.stop_data_rx()
---
+++ pixhawk/static/tabla.js
@@ -0,0 +1,21 @@
+// obtener json de url
+async function get() {
+    let url = '/datos_PX4'
+    let obj = await (await fetch(url)).json();  
+    return obj;
+}
+
+    // funcion para actualizar tabla
+    var datos_PX4,orient;
+    const actualizar = function () {
+    (async () => {
+        datos_PX4 = await get()
+        orient = datos_PX4.attitude.yaw*180/3.141592
+        document.getElementById("yaw").innerHTML = orient.toFixed(1);
+        document.getElementById("speed").innerHTML = datos_PX4.velocity.vz;
+        document.getElementById("time").innerHTML = datos_PX4.time_boot;
+    })()
+    }
+
+    // realiza una accion cada 50 ms
+    var intervalID = window.setInterval(actualizar,50)
