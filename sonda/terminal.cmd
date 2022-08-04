74837f066608ba8d9e8bb3699817b99d3f974035
SebastianMerino <sebastian.merino@pucp.edu.pe>
2022-08-04 18:16:35 -0500

    modified:   sonda/terminal.cmd

--- sonda/terminal.cmd
+++ sonda/terminal.cmd
@@ -1,5 +1,5 @@
 @echo off
 ECHO Puertos disponibles:
 py -m serial.tools.list_ports
-set /p "port=Enter port: "
+set /p "port=Ingresa el puerto: "
 py -m serial.tools.miniterm %port% 38400 --exit-char 101
