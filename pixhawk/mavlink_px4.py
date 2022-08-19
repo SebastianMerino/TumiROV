4a428ed192c2124b91fca9df097dbcd13f80f058
SebastianMerino <sebastian.merino@pucp.edu.pe>
2022-08-19 17:31:13 -0500

    modified:   pixhawk/mavlink_px4.py 	deleted:    ROV_web/puertosUSB.py

--- pixhawk/mavlink_px4.py
+++ pixhawk/mavlink_px4.py
@@ -1,21 +1,29 @@
 from vehicle import Vehicle
 from puertosUSB import buscar_puerto
+import time
 
 puerto = buscar_puerto('MAVLink')
 PX4 = Vehicle(puerto)
 
-PX4.arm()
+# PX4.arm()
+# try:
+#     while True:
+#         canal = input('Canal: ')
+#         pwm = input('PWM en us: ')
+#         PX4.set_servo_pwm(int(canal),int(pwm))
+#         pass
+# finally:
+#     PX4.disarm()
+#     PX4.close_conn()
+
+PX4.start_data_rx()
 try:
     while True:
-        canal = input('Canal: ')
-        pwm = input('PWM en us: ')
-        PX4.set_servo_pwm(int(canal),int(pwm))
-        pass
-finally:
-    PX4.disarm()
-    PX4.close_conn()
+        time.sleep(0.5)
+        print('Velocity: ',PX4.velocity)
+        print('Attitude: ', PX4.attitude)
 
-# PX4.start_data_rx()
-# Aquí puedes recibir datos
-# PX4.stop_data_rx()
+finally:
+    PX4.stop_data_rx()
+    PX4.close_conn()
 
